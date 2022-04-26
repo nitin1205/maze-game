9e2f4c63b077ea031bef46b8aa164ea336267791
ShadyCoder777 <99088343+ShadyCoder777@users.noreply.github.com>
2022-04-26 23:03:18 +0530

    made basic background

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Engine, Render, Runner , World, Bodies, MouseConstraint, Mouse } = Matter;
 
-const width = 800;
+const width = 600;
 const height = 600;
 
 const engine = Engine.create();
@@ -20,35 +20,12 @@ World.add(world, MouseConstraint.create(engine, {
     mouse: Mouse.create(render.canvas)
 }));
 
-// const shape = Bodies.rectangle(200, 200, 50, 50, {
-//     isStatic: true
-// });
-// World.add(world, shape);
 
-// walls / border
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-    Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-    Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-    Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
 ];
 World.add(world, walls)
 
-// random shapes
-for (let i = 0; i < 20; i++) {
-    if (Math.random() < 0.5) {
-        World.add(
-            world,
-            Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
-            );
-    } else {
-        World.add(
-            world,
-            Bodies.circle(Math.random() * width, Math.random() * height, 35, {
-                render: {
-                    fillStyle: 'purple'
-                }
-            })
-        );
-    }
-}
